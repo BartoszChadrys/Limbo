c555494ff0a48422656805b79509e75edc9526b9
BartoszChadrys <bartekch322@gmail.com>
2023-04-20 23:04:59 +0200

    Add login and register button

--- Limbo/ContentView.swift
+++ Limbo/ContentView.swift
@@ -15,20 +15,55 @@ struct ContentView: View {
                 .scaledToFill()
                 .ignoresSafeArea()
             VStack() {
-                Text("Limbo")
-                    .font(.custom("Montserrat", size: 90))
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
                 VStack {
-                    Text("Nauka programowania w C \n Aplikacja e-learningowa \n Politechniki Łódzkiej")
-                        .font(.custom("Montserrat", size: 20))
-                        .fontWeight(.medium)
+                    Text("Limbo")
+                        .font(.custom("Montserrat", size: 100))
+                        .fontWeight(.semibold)
                         .foregroundColor(.white)
-                        .multilineTextAlignment(.trailing)
-                }
+                        
+                    VStack {
+                        Text("Nauka programowania w C \n Aplikacja e-learningowa \n Politechniki Łódzkiej")
+                            .font(.custom("Montserrat", size: 20))
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.trailing)
+                    }
+                } .padding(.top, 70)
                 
+                
+                Spacer()
+                
+                VStack {
+                    Button() {
+                        print("Tapped gradient button")
+                    } label: {
+                        ZStack {
+                            LinearGradient(colors: [Color("redGradientColor"),Color("yellowGradientColor")], startPoint: .leading, endPoint: .trailing)
+                            
+                            Text("Zaloguj się")
+                                .font(.custom("Montserrat", size: 20))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        }
+                        
+                    }
+                    .cornerRadius(25)
+                    .frame(width: 250, height: 60)
+                    
+                    Button() {
+                        print("Tapped darker button")
+                    } label: {
+                        Text("Załóż konto")
+                            .font(.custom("Montserrat", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 250, height: 60)
+                    .background(Color("backgroundColor"))
+                    .cornerRadius(25)
+                }
+                .padding(.bottom, 90)
             }
-            Spacer()
         }
     }
 }
