d0126561a8d3c432bf40ed36d2d33901d5f97006
BartoszChadrys <bartekch322@gmail.com>
2023-04-20 22:06:39 +0200

    Initial Commit

---
+++ Limbo/LimboApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimboApp.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LimboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
