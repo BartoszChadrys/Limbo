fafb4b433ebaa79240f629d5549df51589a23f04
BartoszChadrys <bartekch322@gmail.com>
2023-04-20 22:45:28 +0200

    Add Limbo text with description

--- Limbo/ContentView.swift
+++ Limbo/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
+        ZStack() {
             Image("backgroundFlames")
                 .resizable()
-                .aspectRatio(contentMode: .fill)
+                .scaledToFill()
                 .ignoresSafeArea()
+            VStack() {
+                Text("Limbo")
+                    .font(.custom("Montserrat", size: 90))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Nauka programowania w C \n Aplikacja e-learningowa \n Politechniki Łódzkiej")
+                        .font(.custom("Montserrat", size: 20))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.trailing)
+                }
+                
+            }
+            Spacer()
         }
     }
 }
