05d02dfc05ea4eec599cb68a2c56cedd46413d00
BartoszChadrys <bartekch322@gmail.com>
2023-04-20 22:16:36 +0200

    Add background flames

--- Limbo/ContentView.swift
+++ Limbo/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Image("backgroundFlames")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
