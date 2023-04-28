0ca19c3dc6b3a4a760fba9cae94ab060f395d88a
Bartosz Chadryś <bartekch322@gmail.com>
2023-04-28 12:16:27 +0200

    Add the best in the group section

--- Limbo/Views/HomeView/HomeView.swift
+++ Limbo/Views/HomeView/HomeView.swift
@@ -8,42 +8,67 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var viewModel = HomeViewModel()
+    
     var body: some View {
         ZStack {
             backgroundColorView()
             
-            HStack(spacing: 20) {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 65, height: 35)
-                        .foregroundColor(Color("backgroundBoxColor"))
-                        .cornerRadius(20)
-                    HStack() {
-                        Image("flame")
+            VStack {
+                HStack(spacing: 20) {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 65, height: 35)
+                            .foregroundColor(Color("backgroundBoxColor"))
+                            .cornerRadius(20)
+                        HStack() {
+                            Image("flame")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 20, alignment: .leading)
+                            Text("50")
+                                .font(.custom("Montserrat", size: 14))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .padding(.top, 50)
+                    
+                        LimboLogoView()
+                    
+                        Image("exampleAvatar")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20, alignment: .leading)
-                        Text("50")
-                            .font(.custom("Montserrat", size: 14))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
+                            .frame(width: 35, height: 35)
+                            .padding(.top, 50)
                     }
+                    .padding(.top, 50)
+                
+                VStack(alignment: .leading) {
+                    Text("Najlepsi w grupie")
+                        .font(.custom("Montserrat", size: 20))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack(spacing: 25) {
+                            ForEach(viewModel.imageList, id: \.self) { image in
+                                Image(image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 65, height: 65)
+                            }
+                        }
+                    }
+                    .frame(maxHeight: 65)
                 }
-                .padding(.top, 50)
-                    LimboLogoView()
-                    Image("exampleAvatar")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 35, height: 35)
-                        .padding(.top, 50)
+                    
+                
+                Spacer()
                 
                 }
-                .padding(.top, 50)
-                
             }
             
-            
-            Spacer()
             
             
         }
