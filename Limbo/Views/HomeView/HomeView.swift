3009676877d987cf89ee16fe5e9880fe709355fa
Bartosz Chadryś <bartekch322@gmail.com>
2023-04-28 10:57:02 +0200

    Add logo, avatar and flickers to HomeView

--- Limbo/Views/HomeView/HomeView.swift
+++ Limbo/Views/HomeView/HomeView.swift
@@ -9,9 +9,46 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        backgroundColorView()
+        ZStack {
+            backgroundColorView()
+            
+            HStack(spacing: 20) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 65, height: 35)
+                        .foregroundColor(Color("backgroundBoxColor"))
+                        .cornerRadius(20)
+                    HStack() {
+                        Image("flame")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20, alignment: .leading)
+                        Text("50")
+                            .font(.custom("Montserrat", size: 14))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                    }
+                }
+                .padding(.top, 50)
+                    LimboLogoView()
+                    Image("exampleAvatar")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 35, height: 35)
+                        .padding(.top, 50)
+                
+                }
+                .padding(.top, 50)
+                
+            }
+            
+            
+            Spacer()
+            
+            
+        }
     }
-}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
