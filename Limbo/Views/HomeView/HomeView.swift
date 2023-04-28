f22a095bcd61355070299ee386522b0483ab2959
BartoszChadrys <bartekch322@gmail.com>
2023-04-28 10:30:08 +0200

    Add HomeView and HomeViewModel

---
+++ Limbo/Views/HomeView/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 28/04/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        backgroundColorView()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Limbo/Views/HomeView/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 28/04/2023.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+}
