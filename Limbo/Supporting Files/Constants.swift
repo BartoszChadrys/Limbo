2a09539e8fc149abba0bbe0cb50fb853120d624b
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-03 17:05:29 +0200

    Add Constants file with various gradients

---
+++ Limbo/Supporting Files/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 03/05/2023.
+//
+
+import SwiftUI
+
+struct K {
+    static let orangeGradient = LinearGradient(colors: [Color("redGradientColor"),Color("yellowGradientColor")], startPoint: .leading, endPoint: .trailing)
+    static let greenGradient = LinearGradient(colors: [Color("lightGreenColor"), Color("darkGreenColor")], startPoint: .leading, endPoint: .trailing)
+    static let redGradient = LinearGradient(colors: [Color("lightRedColor"), Color("darkRedColor")], startPoint: .leading, endPoint: .trailing)
+}
