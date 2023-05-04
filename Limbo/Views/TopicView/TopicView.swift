c04bcc7ed5482033ea2ae23526e0adf139dd5cc6
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-04 17:13:31 +0200

    Add TopicCircleView

--- Limbo/Views/TopicView/TopicView.swift
+++ Limbo/Views/TopicView/TopicView.swift
@@ -11,7 +11,7 @@ struct TopicView: View {
     var body: some View {
         ZStack {
             backgroundColorView()
-            VStack {
+            VStack() {
                 LimboLogoWithPointsView()
                 
                 Text("Rozdziały")
@@ -27,24 +27,22 @@ struct TopicView: View {
                         .padding(.leading, 25)
                     
                     HStack(spacing: 40) {
-                        VStack(spacing: 10) {
-                            Text("Operacje na danych")
-                                .font(.custom("Montserrat", size: 15))
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
-                            
-                            Text("Zdobyte punkty")
-                                .font(.custom("Montserrat", size: 12))
-                                .fontWeight(.medium)
-                                .foregroundColor(.white)
-                            
-                            Text("15/15")
-                                .font(.custom("Montserrat", size: 20))
-                                .fontWeight(.semibold)
-                                .foregroundColor(Color("greenColor"))
-                        }
-                        
-                        TopicCircleView(percent: 100, title: "Gratulacje!", gradient: K.greenGradient, circleColor: Color("greenColor"), circleIcon: Image("checkmarkIcon"))
+                        TopicTitleView(title: "Operacje na danych", points: 15, pointsColor: Color("greenColor"))
+                        TopicCircleView(percent: 100, title: "Gratulacje!", gradient: K.greenGradient, mainColor: Color("greenColor"), circleIcon: Image("checkmarkIcon"))
+                    }
+                }
+                .padding(.bottom, 15)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(K.orangeGradient, lineWidth: 3)
+                        .frame(height: 120)
+                        .padding(.trailing, 25)
+                        .padding(.leading, 25)
+                    
+                    HStack(spacing: 40) {
+                        TopicTitleView(title: "Instrukcje warunkowe", points: 7, pointsColor: Color("orangeColor"))
+                        TopicCircleView(percent: 47, title: "Gratulacje!", gradient: K.orangeGradient, mainColor: Color("orangeColor"), circleIcon: Image("checkmarkIcon"))
                     }
                 }
                 
@@ -64,18 +62,22 @@ struct TopicCircleView: View {
     var percent: Int
     var title: String
     var gradient: LinearGradient
-    var circleColor: Color
+    var mainColor: Color
     var circleIcon: Image
     
     var body: some View {
         ZStack {
+            Circle()
+                .foregroundColor(Color("backgroundColor"))
+                .frame(width: 75, height: 75)
+                .shadow(color: mainColor,radius: 15)
+            
             RoundedRectangle(cornerRadius: 50)
                 .stroke(gradient, lineWidth: 4.5)
-                .foregroundColor(.white)
                 .frame(width: 75, height: 75)
             
             RoundedRectangle(cornerRadius: 50)
-                .foregroundColor(circleColor)
+                .foregroundColor(mainColor)
                 .frame(width: 20, height: 20)
                 .padding(.bottom, 75)
             
@@ -96,6 +98,31 @@ struct TopicCircleView: View {
                     .fontWeight(.light)
                     .foregroundColor(.white)
             }
+        }
+    }
+}
+
+struct TopicTitleView: View {
+    var title: String
+    var points: Int
+    var pointsColor: Color
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(title)
+                .font(.custom("Montserrat", size: 15))
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            
+            Text("Zdobyte punkty")
+                .font(.custom("Montserrat", size: 12))
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+            
+            Text("\(points)/15")
+                .font(.custom("Montserrat", size: 20))
+                .fontWeight(.semibold)
+                .foregroundColor(pointsColor)
         }
     }
 }
