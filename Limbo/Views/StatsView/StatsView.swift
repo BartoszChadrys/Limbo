0929511bae5b779e78a64ded4ecafc0c3cfb1cba
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-04 21:14:37 +0200

    Add StatsView and StatsViewModel

---
+++ Limbo/Views/StatsView/StatsView.swift
@@ -0,0 +1,25 @@
+//
+//  StatsView.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 04/05/2023.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        ZStack {
+            backgroundColorView()
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct StatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatsView()
+    }
+}
---
+++ Limbo/Views/StatsView/StatsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  StatsViewModel.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 04/05/2023.
+//
+
+import Foundation
+
+class StatsViewModel: ObservableObject {
+    
+}
