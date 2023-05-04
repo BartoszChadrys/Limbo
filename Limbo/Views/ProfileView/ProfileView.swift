b788a2691a2b7b6dde56929191afd4b88e79a835
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-04 22:30:36 +0200

    Add avatar and mail

--- Limbo/Views/ProfileView/ProfileView.swift
+++ Limbo/Views/ProfileView/ProfileView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject private var viewModel = ProfileViewModel()
+    
     var body: some View {
         ZStack {
             backgroundColorView()
+            
+            VStack(spacing: 15) {
+                LimboLogoWithPointsView()
+                
+                VStack(spacing: 0) {
+                    Image("exampleAvatar")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    
+                    Image("arrowsRotateIcon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 17, height: 17)
+                        .padding(.leading, 85)
+                }
+                
+                Text("Maciej Kowalski")
+                    .font(.custom("Montserrat", size: 20))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .foregroundColor(Color("backgroundBoxColor"))
+                        .frame(width: 230, height: 45)
+                    
+                    HStack(spacing: 25) {
+                        Image("envelopeIconGradient")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                        
+                        Text("maciejk@gmail.com")
+                            .font(.custom("Montserrat", size: 12))
+                            .fontWeight(.light)
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                Spacer()
+                
+                NavbarView(homeIcon: "homeIconWhite", quizIcon: "quizIconWhite", statsIcon: "statsIconWhite", profileIcon: "profileIconGradient")
+            }
         }
     }
 }
