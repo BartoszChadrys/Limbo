fe99b08eaedbdf1750b637400fbd3a56490343d0
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-16 21:15:58 +0200

    Add proper background, offset and foregroundColor to icons

---
+++ Limbo/Views/CustomTabBar.swift
@@ -0,0 +1,43 @@
+//
+//  CustomNavbar.swift
+//  Limbo
+//
+//  Created by Bartek Chadryś on 16/05/2023.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    @Binding var currentTab: Tab
+    
+    var body: some View {
+        VStack() {
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Button() {
+                        withAnimation(.easeInOut) {
+                            currentTab = tab
+                        }
+                    } label: {
+                        Image(systemName: tab.rawValue)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(currentTab == tab ? .orangeColor() : .white)
+                            .offset(y: currentTab == tab ? -10 : 0)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .frame(height: 24)
+        .padding(.top, 30)
+        .background(Color.backgroundBoxColor())
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
