75ac1082a7b6fc9cde179d55a413390fe73643ff
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-23 18:35:26 +0200

    Add StatsRectangleView

--- Limbo/Views/StatsView/StatsView.swift
+++ Limbo/Views/StatsView/StatsView.swift
@@ -13,10 +13,27 @@ struct StatsView: View {
     var body: some View {
         ZStack {
             backgroundColorView()
-            VStack {
+            VStack(spacing: 15) {
                 LimboLogoWithPointsView()
                 
-                ActivityChartView(activityModel: activityModel)
+                VStack {
+                    Text("Twoja aktywność w tym tygodniu")
+                        .font(.custom("Montserrat", size: 14))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .padding(3)
+                    
+                    ActivityChartView(activityModel: activityModel)
+                }
+                
+                VStack(spacing: 15) {
+                    StatsRectangleView(title: "Wykonanych pytań", score: "25")
+                    StatsRectangleView(title: "Ukończonych działów", score: "1")
+                    StatsRectangleView(title: "Średni czas na pytanie", score: "5,5s")
+                    StatsRectangleView(title: "Najwięcej pytań w ciągu dnia", score: "15")
+                }
+                .padding(.top, 15)
+                
                 
                 Spacer()                
             }
@@ -27,5 +44,37 @@ struct StatsView: View {
 struct StatsView_Previews: PreviewProvider {
     static var previews: some View {
         StatsView()
+    }
+}
+
+struct StatsRectangleView: View {
+    var title: String
+    var score: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(LinearGradient.orangeGradient(), lineWidth: 3)
+                .frame(maxHeight: 50)
+                .padding(.leading, 15)
+                .padding(.trailing, 15)
+            
+            HStack() {
+                Text(title)
+                    .font(.custom("Montserrat", size: 12))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 35)
+                
+                Text(score)
+                    .font(.custom("Montserrat", size: 14))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 45)
+            }
+            
+        }
     }
 }
