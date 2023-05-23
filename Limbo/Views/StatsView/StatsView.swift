45a7b714655103d465280a81fc22989e2d91a4ad
Bartosz Chadryś <bartekch322@gmail.com>
2023-05-23 18:52:06 +0200

    Add BonusRectangleView

--- Limbo/Views/StatsView/StatsView.swift
+++ Limbo/Views/StatsView/StatsView.swift
@@ -26,13 +26,30 @@ struct StatsView: View {
                     ActivityChartView(activityModel: activityModel)
                 }
                 
-                VStack(spacing: 15) {
+                VStack(spacing: 20) {
                     StatsRectangleView(title: "Wykonanych pytań", score: "25")
                     StatsRectangleView(title: "Ukończonych działów", score: "1")
                     StatsRectangleView(title: "Średni czas na pytanie", score: "5,5s")
                     StatsRectangleView(title: "Najwięcej pytań w ciągu dnia", score: "15")
                 }
                 .padding(.top, 15)
+                
+                VStack {
+                    Text("Otrzymane bonusy")
+                        .font(.custom("Montserrat", size: 16))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .padding(.top, 10)
+                        .padding(.bottom, 10)
+                    
+                    HStack(spacing: 35) {
+                        BonusRectangleView(title: "Dodatkowe punkty na egzaminie", points: 21)
+                            .padding(.leading, 15)
+                        
+                        BonusRectangleView(title: "Dodatkowe punkty w Dante", points: 37)
+                            .padding(.trailing, 15)
+                    }
+                }
                 
                 
                 Spacer()                
@@ -54,8 +71,8 @@ struct StatsRectangleView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .stroke(LinearGradient.orangeGradient(), lineWidth: 3)
-                .frame(maxHeight: 50)
+                .stroke(LinearGradient.orangeGradient(), lineWidth: 1.5)
+                .frame(height: 50)
                 .padding(.leading, 15)
                 .padding(.trailing, 15)
             
@@ -75,6 +92,32 @@ struct StatsRectangleView: View {
                     .padding(.trailing, 45)
             }
             
+        }
+    }
+}
+
+struct BonusRectangleView: View {
+    var title: String
+    var points: Int
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(LinearGradient.orangeGradient(), lineWidth: 1.5)
+                .frame(height: 120)
+            
+            VStack(spacing: 5) {
+                Text("+\(points)")
+                    .font(.custom("Montserrat", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text(title)
+                    .font(.custom("Montserrat", size: 10))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            }
         }
     }
 }
